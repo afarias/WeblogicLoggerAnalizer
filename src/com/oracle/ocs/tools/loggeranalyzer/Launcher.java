b76d0672ac7161a4dc2e3d4889e4191f107e21b0
Andres Farias <andres.farias@oracle.com>
2017-05-18 11:05:19 -0400

    Initial commit

---
+++ src/com/oracle/ocs/tools/loggeranalyzer/Launcher.java
@@ -0,0 +1,13 @@
+package com.oracle.ocs.tools.loggeranalyzer;
+
+/**
+ * This class aims to easy the launching process.
+ *
+ * @author Andrés Farías on 5/18/17.
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        
+    }
+}
