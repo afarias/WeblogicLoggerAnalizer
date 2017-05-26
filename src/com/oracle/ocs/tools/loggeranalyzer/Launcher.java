e1acc96e16ecd85ba07a8a631a61a074616b0c90
Andres Farias <andres.farias@oracle.com>
2017-05-26 11:25:23 -0400

    Se configura Log4J correctamente. Se crea una clase qeu representa el log.

--- src/com/oracle/ocs/tools/loggeranalyzer/Launcher.java
+++ src/com/oracle/ocs/tools/loggeranalyzer/Launcher.java
@@ -1,5 +1,14 @@
 package com.oracle.ocs.tools.loggeranalyzer;
 
+import org.apache.log4j.lf5.util.LogFileParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.net.URL;
+
 /**
  * This class aims to easy the launching process.
  *
@@ -7,7 +16,23 @@ package com.oracle.ocs.tools.loggeranalyzer;
  */
 public class Launcher {
 
+    private static Logger logger = LoggerFactory.getLogger(Launcher.class);
+
     public static void main(String[] args) {
-        
+
+        URL resource = new Thread().getContextClassLoader().getResource("log4j.xml");
+        /* The file is chosen */
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File("/temp/logs"));
+
+        /* The Dialog is opened */
+        Component parent = new JFrame("WebLogic Logger Analyzer");
+        int result = fileChooser.showOpenDialog(parent);
+
+        logger.debug("Resultado del diálogo: " + result);
+        logger.debug("Archivo recuperado: " + fileChooser.getSelectedFile().getName());
+
+        /* Se crea el objeto que representa el Log */
+        LogFile logFile = new LogFile(fileChooser.getSelectedFile());
     }
 }
---
+++ src/com/oracle/ocs/tools/loggeranalyzer/LogFile.java
@@ -0,0 +1,11 @@
+package com.oracle.ocs.tools.loggeranalyzer;
+
+import java.io.File;
+
+/**
+ * @author Andrés Farías on 5/26/17.
+ */
+public class LogFile {
+    public LogFile(File selectedFile) {
+    }
+}
